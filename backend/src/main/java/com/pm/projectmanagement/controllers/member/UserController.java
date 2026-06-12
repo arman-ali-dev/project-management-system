39afae56aa3f5e8951dd1e9c96a85ce71fb3fe6a
Arman Ali <armaanali.dev@gmail.com>
2026-06-12 13:17:52 +0530

    Update UserController.java

--- backend/src/main/java/com/pm/projectmanagement/controllers/member/UserController.java
+++ backend/src/main/java/com/pm/projectmanagement/controllers/member/UserController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
@@ -32,6 +34,12 @@ public class UserController {
         User updateProfile = userService.updateProfile(existingUser, user);
 
         return new ResponseEntity<>(updateProfile, HttpStatus.OK);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<User>> getAllUsersHandler() {
+        List<User> users = userService.getAllUsers();
+        return new ResponseEntity<>(users, HttpStatus.OK);
     }
 
 }
